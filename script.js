9872b9912b58c01ddbbb6571a4896f5bf4ea3849
winterlucaso <58354096+winterlucaso@users.noreply.github.com>
2023-09-04 20:48:56 -0500

    add functionality to removeBtn

--- script.js
+++ script.js
@@ -3,35 +3,38 @@
 // - link reader stats to individual book stats
 // - add a "remove" button to book cards
 
+const myLibrary = [];
+
 const form = document.getElementById('form');
 const table = document.querySelector("table");
+const booksContainer = document.querySelector("#container-books");
 
 const addBookBtn = document.getElementById("addBookBtn");
 const addBookDialog = document.getElementById("addBookDialog");
 const confirmBtn = addBookDialog.querySelector("#confirmBtn");
 const cancelBtn = addBookDialog.querySelector("#cancelBtn");
 
-const myLibrary = [];
+const totalBooks = document.querySelector("#total-books");
 
 // -- Book Constructor --
 class Book {
-    constructor (title, author, pageCount, isRead) {
+    constructor (title, author, pageCount, isRead, id) {
         this.title = title;
         this.author = author;
         this.pageCount = pageCount;
         this.isRead = isRead;
+        this.id = id;
     }
 }
 
 // -- Form Stuff --
-// Receive Book Button
+// Add Book Button
 addBookBtn.addEventListener("click", () => {
     addBookDialog.showModal();
 });
 
+// Confirm Button
 confirmBtn.addEventListener('click', (e) => {
-    console.log("submit recognized");
-
     // Receive form inputs
     const bookTitle = document.getElementById("book_title");
     const bookAuthor = document.getElementById("book_author");
@@ -42,7 +45,8 @@ confirmBtn.addEventListener('click', (e) => {
     let book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookReadStatus.value);
 
     addBookToLibrary(book); // add to myLibrary array
-    addBookCard(book); // add card to DOM
+    clearLibraryDOM(); // clear DOM
+    addLibraryToDOM(); // reload DOM from myLibrary array
 
     // Clears form
     bookTitle.value = "";
@@ -55,6 +59,7 @@ confirmBtn.addEventListener('click', (e) => {
     e.preventDefault(); // We don't want to submit this fake form
 });
 
+// Cancel Button
 cancelBtn.addEventListener('click', (e) => {
     addBookDialog.close();
     // e.preventDefault();
@@ -62,35 +67,78 @@ cancelBtn.addEventListener('click', (e) => {
 
 // Add book to myLibrary array
 function addBookToLibrary(book) {
-  myLibrary.push(book);
-  console.log(myLibrary);
+    myLibrary.push(book);
+    console.log("Adding '" + book.title + "' at index: " + (myLibrary.length - 1))
+    console.log(myLibrary);
 }
 
-// Create book card and add to DOM
-function addBookCard(book) {
-    const bookRow = document.createElement('tr');
-    // bookRow.classList.add("book");
-    table.appendChild(bookRow);
+// clears DOM
+function clearLibraryDOM() {
+    while (booksContainer.firstChild) {
+        booksContainer.removeChild(booksContainer.lastChild);
+    }
+}
 
-    const bookTitle = document.createElement('td');
-    // bookTitle.classList.add("book-title");
+// adds entire myLibrary to DOM
+function addLibraryToDOM() {
+    for (let book = 0; book < myLibrary.length; book++) {
+        myLibrary[book].id = book; // assign a data value to the book
+        addBookToDOM(myLibrary[book]);
+    }
+}
+
+// Create HTML book elements and add to DOM
+function addBookToDOM(book) {
+    const bookRow = document.createElement('tr'); // Table row
+    booksContainer.appendChild(bookRow);
+
+    const bookTitle = document.createElement('td'); // Title
     bookTitle.innerText = book.title;
     bookRow.append(bookTitle);
 
-    const bookAuthor = document.createElement('td');
-    // bookAuthor.classList.add("book-author");
+    const bookAuthor = document.createElement('td'); // Author
     bookAuthor.innerText = book.author;
     bookRow.append(bookAuthor);
 
-    const bookPageCount = document.createElement('td');
-    // bookPageCount.classList.add("book-page-count");
+    const bookPageCount = document.createElement('td'); // Page count
     bookPageCount.innerText = book.pageCount;
     bookRow.append(bookPageCount);
 
-    const bookReadStatus = document.createElement('td');
-    // bookReadStatus.classList.add("book-read-status");
+    const bookReadStatus = document.createElement('td'); // Read status
     bookReadStatus.innerText = book.isRead;
     bookRow.append(bookReadStatus);
 
-
+    const bookRemoveCell = document.createElement('td'); // RemoveBtn
+    const bookRemoveBtn = document.createElement('button');
+    bookRemoveBtn.classList.add("remove-btn");
+    bookRemoveBtn.innerText = "X";
+    bookRemoveBtn.dataset.bookNum = book.id; // Attaches data value to the removeBtn
+    bookRemoveBtn.addEventListener('click', (e) => { // Adds event listener to removeBtn
+        console.log("Removing '" + book.title + "' at index: " + bookRemoveBtn.dataset.bookNum);
+        removeBookFromLibrary(bookRemoveBtn.dataset.bookNum); // Removes book
+    });
+    bookRow.append(bookRemoveCell);
+    bookRemoveCell.append(bookRemoveBtn);
 }
+
+// Removes book from myLibrary and rebuilds DOM
+function removeBookFromLibrary(num) {
+    myLibrary.splice(num, 1);
+    clearLibraryDOM();
+    addLibraryToDOM();
+    updateStats();
+}
+
+function updateStats() {
+    totalBooks.innerText = myLibrary.length;
+}
+
+// Script
+
+// Starter Book
+let starterBook = new Book("The Hobbit", "J. R. R. Tolkein", 310, "Read");
+
+addBookToLibrary(starterBook); // add to myLibrary array
+clearLibraryDOM(); // clear DOM
+addLibraryToDOM(); // reload DOM from myLibrary array
+updateStats();
